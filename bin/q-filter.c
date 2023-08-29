afebf205a44e31eee02d9b3258e9c739148b1078
marcus-hoy-hansen <124658804+marcus-hoy-hansen@users.noreply.github.com>
2023-08-29 13:27:50 +0200

    Update q-filter.c
    Cleaning code

--- bin/q-filter.c
+++ bin/q-filter.c
@@ -1,64 +1,53 @@
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
 #include <stdlib.h>
 
 int main(int argc,char* argv[]) {
 
-  int lowerlength = 200;
+  	int lowerlength = 200;
 	int lowerqual = 20;
-	if(argc>1) {
-		lowerqual=atoi(argv[2]);
-	}
+	
+	if(argc>1) lowerqual=atoi(argv[2]);
 
-	if (argc>2) {
-		lowerlength=atoi(argv[3]);
-	}
+	if (argc>2) lowerlength=atoi(argv[3]);
 
-
-	FILE *fp;
-	int len = 100000;
-	char buff[len], ch, qual[len], dna[len], plus[len], id[len];
+	FILE *fastqfile;
+	int len = 10000;
+	char buff[len], currentchar, qual[len], dna[len], plus[len], id[len];
 	int lines = 0;
-	fp = fopen(argv[1], "r");
+	fastqfile = fopen(argv[1], "r");
 	int i = 0;
 	int u;
 	double score = 0.00;
 	int mean;
 	
-	while(!feof(fp)) {
-		ch = fgetc(fp);
-		if(ch == '\n') {
-			lines++;
-		}
-	}
+	while(!feof(fastqfile)) {
+		currentchar = fgetc(fastqfile);
+		
+		if(currentchar == '\n') lines++;
 	
-	printf("%i",lines);
-	rewind(fp);
+	}	
+	rewind(fastqfile);
 	
 	while (i <= lines/4) {
 		i++;
-		fgets(buff, len, fp);
+		fgets(buff, len, fastqfile);
 		strcpy(id, buff);
-		fgets(buff, len, fp);
+		fgets(buff, len, fastqfile);
 		strcpy(dna, buff);
-		fgets(buff, len, fp);
-		strcpy(plus, buff);
-		fgets(buff, len, fp);
+		fgets(buff, len, fastqfile);
+		fgets(buff, len, fastqfile);
 		strcpy(qual, buff);
 		score = 0;
 		for (u = 0; u < strlen(buff); u++) {
-			score = score + (buff[u]-33);
+			score += (buff[u]-33);
 		}
 	
 		mean = (score/strlen(buff));
-		if(mean>=lowerqual && strlen(dna)>lowerlength) {
-			printf("%s", id);
-			printf("%s", dna);
-			printf("%s", plus);
-			printf("%s", qual);
+		if(mean>=lowerqual && strlen(dna)>=lowerlength) {
+			printf("%s%s+\n%s", id, dna, qual);
 		}
 	}
-	fclose(fp);
+	fclose(fastqfile);
 	return 0;
 }
