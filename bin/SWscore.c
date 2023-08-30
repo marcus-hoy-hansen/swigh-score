11f7b793649b1090bdf1a5a9cb63ca822e784950
marcus-hoy-hansen <124658804+marcus-hoy-hansen@users.noreply.github.com>
2023-08-30 22:34:01 +0200

    Update SWscore.c
    cleaning

--- bin/SWscore.c
+++ bin/SWscore.c
@@ -1,115 +1,117 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-// gcc SWscore.c -o SWscore
- 
-int main(int argc,char* argv[])
-{
-	int match = 1;
-	int val = 0;
+int main(int argc, char *argv[]) {
+    // Initialize match and val
+    int match = 1;
+    int val = 0;
 
-	int cnum1 = strlen(argv[1]);
-	char str1[cnum1+10];
-	int cnum2 = strlen(argv[2]);
-	char str2[cnum2+10];
-	double SWpercent;
+    // Get lengths of input strings
+    int len1 = strlen(argv[1]);
+    int len2 = strlen(argv[2]);
 
-	strcpy(str1,argv[1]);
-	strcpy(str2,argv[2]);
-	
-	int arr[cnum2][cnum1];
+    // Declare and copy input strings
+    char str1[len1 + 10];
+    char str2[len2 + 10];
+    strcpy(str1, argv[1]);
+    strcpy(str2, argv[2]);
 
-	for(int i=0;i<cnum2;i++){
-		for(int j=0;j<cnum1;j++){
-			if(str1[j]==str2[i])
-				arr[i][j]=1;
-			else
-				arr[i][j]=0;
-		}
-	}
+    // Calculate SWpercent
+    double SWpercent;
 
-	int c;
-	int ul;
-	int l;
-	int u;
-	int maxc;
-	int maxr;
+    // Create and initialize the scoring matrix
+    int scoreMatrix[len2][len1];
+    for (int i = 0; i < len2; i++) {
+        for (int j = 0; j < len1; j++) {
+            scoreMatrix[i][j] = (str1[j] == str2[i]) ? 1 : 0;
+        }
+    }
 
-	for(int i=1;i<cnum2;i++){
-		for(int j=1;j<cnum1;j++){
-			
-			c=arr[i][j];
-			ul=arr[i-1][j-1];
-			l=arr[i][j-1];
-			u=arr[i-1][j];
-			
-			if( c>0 && (ul+1)>(l-1) && (ul+1)>(u-1)) {
-				arr[i][j] = ul+1;
-			
-			} else if( c==0 && (ul+1)>(l-1) && (ul+1)>(u-1)) {
-				arr[i][j] = ul-1;
+    // Initialize variables for traceback
+    int currentCell, upperLeft, left, up;
+    int maxRow, maxCol;
 
-			} else if((l-1)>(u-1)) {
-				arr[i][j] = l-1;
+    // Perform Smith-Waterman algorithm
+    for (int i = 1; i < len2; i++) {
+        for (int j = 1; j < len1; j++) {
+            currentCell = scoreMatrix[i][j];
+            upperLeft = scoreMatrix[i - 1][j - 1];
+            left = scoreMatrix[i][j - 1];
+            up = scoreMatrix[i - 1][j];
 
-			} else if((u-1)>(l-1)) {
-				arr[i][j] = u-1;
-			} else { 
-				arr[i][j] = 0;
-			}
-			
-			if(arr[i][j]<0) arr[i][j] = 0;
-			
-			if(arr[i][j]>val) {
-				val = arr[i][j];
-				maxr = i;
-				maxc = j;
-			}	
-		}
-	}
-	int crow = maxr;
-	int ccol = maxc;
+            // Update current cell based on traceback rules
+            if (currentCell > 0 && upperLeft + 1 > left - 1 && upperLeft + 1 > up - 1) {
+                scoreMatrix[i][j] = upperLeft + 1;
+            } else if (currentCell == 0 && upperLeft + 1 > left - 1 && upperLeft + 1 > up - 1) {
+                scoreMatrix[i][j] = upperLeft - 1;
+            } else if (left - 1 > up - 1) {
+                scoreMatrix[i][j] = left - 1;
+            } else if (up - 1 > left - 1) {
+                scoreMatrix[i][j] = up - 1;
+            } else {
+                scoreMatrix[i][j] = 0;
+            }
 
-	char alignment1[cnum1];
-	char alignment2[cnum2];
-	char alignmentmatch[1000];
-	int alignpos = 0;	
+            // Update max value and position
+            if (scoreMatrix[i][j] > val) {
+                val = scoreMatrix[i][j];
+                maxRow = i;
+                maxCol = j;
+            }
+        }
+    }
 
-	for(int i=val;i>0;i--){
-		
-		if(str1[ccol]==str2[crow]) {
-                        match=match+1;
-                        alignmentmatch[alignpos] = '|';
-                        alignment1[alignpos] = str1[ccol];
-                        alignment2[alignpos] = str2[crow];
-                        alignpos = alignpos+1;
-                }
-	
-		ul=arr[crow-1][ccol-1];
-		l=arr[crow][ccol-1];
-		u=arr[crow-1][ccol];
-		
-		if(ul>=l && ul>=u) {
-			crow = crow - 1;
-			ccol = ccol - 1;
-		} else if(l>u) {
-			ccol = ccol - 1;
-		} else {
-			crow = crow - 1;
-		}
-		
-		i = arr[crow][ccol];
-	}
-	alignmentmatch[alignpos + 1] = '\0';
-	alignment1[alignpos + 1] = '\0';
-	alignment2[alignpos + 1] = '\0';
+    // Initialize traceback variables
+    int currentRow = maxRow;
+    int currentCol = maxCol;
 
-	SWpercent = (double)val/(double)match;
+    // Initialize alignment strings
+    char alignment1[len1];
+    char alignment2[len2];
+    char alignmentMatch[1000];
+    int alignPos = 0;
 
-	printf("%s\t", str1);
-	printf("%f\t", SWpercent);
-	printf("%i\t", val);
-	printf("%i\t", match);
-	printf("%f\n", SWpercent*((double)val+(double)match));
-	return 0;
+    // Perform traceback
+    for (int i = val; i > 0; i--) {
+        if (str1[currentCol] == str2[currentRow]) {
+            match++;
+            alignmentMatch[alignPos] = '|';
+            alignment1[alignPos] = str1[currentCol];
+            alignment2[alignPos] = str2[currentRow];
+            alignPos++;
+        }
+
+        upperLeft = scoreMatrix[currentRow - 1][currentCol - 1];
+        left = scoreMatrix[currentRow][currentCol - 1];
+        up = scoreMatrix[currentRow - 1][currentCol];
+
+        // Update current position based on traceback rules
+        if (upperLeft >= left && upperLeft >= up) {
+            currentRow--;
+            currentCol--;
+        } else if (left > up) {
+            currentCol--;
+        } else {
+            currentRow--;
+        }
+
+        i = scoreMatrix[currentRow][currentCol];
+    }
+
+    // Null-terminate alignment strings
+    alignmentMatch[alignPos] = '\0';
+    alignment1[alignPos] = '\0';
+    alignment2[alignPos] = '\0';
+
+    // Calculate SWpercent
+    SWpercent = (double) val / (double) match;
+
+    // Print results
+    printf("%s\t", str1);
+    printf("%f\t", SWpercent);
+    printf("%i\t", val);
+    printf("%i\t", match);
+    printf("%f\n", SWpercent * ((double) val + (double) match));
+
+    return 0;
 }
