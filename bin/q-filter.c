5ff1c5d6af11fca3b67418834dd8fbf0ff2ecdcb
marcus-hoy-hansen <124658804+marcus-hoy-hansen@users.noreply.github.com>
2023-08-30 22:22:00 +0200

    Update q-filter.c

--- bin/q-filter.c
+++ bin/q-filter.c
@@ -5,54 +5,67 @@
 
 int main(int argc,char* argv[]) {
 
-  	int lowerlength = 200;
-	double lowerqual = 20.0;
-	int runmode=1;
-	if(argc>2) lowerqual=atof(argv[2]);
+        int lowerlength = 200;
+        double lowerqual = 20.0;
+        int runmode=1;
+
+	// Parse command-line arguments
+	if (argc > 2) lowerqual = atof(argv[2]);
 	if (argc>3) lowerlength=atoi(argv[3]);
 	if (argc>4) runmode=2;
 
-	double P=pow(10,-lowerqual/10);
-	FILE *fastqfile;
-	int len = 10000;
-	char buff[len], currentchar;
-	char fastqline[4][len];
 	int lines = 0;
-	fastqfile = fopen(argv[1], "r");
-	double score = 0.00;
+	int len = 10000;
+	double P=pow(10,-lowerqual/10);
+	double score = 0.0;
 	double mean;
-	
+	char buff[len], bufflen, currentchar,fastqline[4][len];
+
+	// Open the FASTQ file
+	FILE *fastqfile;
+	fastqfile = fopen(argv[1], "r");
 	while(!feof(fastqfile)) {
 		currentchar = fgetc(fastqfile);
 		
 		if(currentchar == '\n') lines++;
 	
 	}	
-	rewind(fastqfile);
-	for (int i = 0; i < lines/4; i++) {
 
+	// Rewind the file to the beginning
+	rewind(fastqfile);
+	
+	// Process the FASTQ entries
+	for (int i = 0; i < lines/4; i++) {
 		for (int j=1; j <= 4; ++j) {
 			fgets(buff, len, fastqfile);
                 	strcpy(fastqline[j], buff);
 		}
 		
 		score = 0;
+		bufflen = strlen(buff)-1;
+
+		// Calculate score and mean based on runmode
 		if(runmode==1) {
-			for (int u = 0; u < strlen(buff)-1; u++) score += (buff[u]-33);
-			mean = -1*(score/strlen(buff))/10;
+			for (int u = 0; u < bufflen; u++) score += (buff[u]-33);
+			mean = -1*(score/bufflen)/10;
 			mean = pow(10,mean);
 
 		} else {
-			for (int u = 0; u < strlen(buff)-1; u++) {
+			for (int u = 0; u < bufflen; u++) {
 				score = score+pow(10,-1*(double)(buff[u]-33)/10);
 			}	
 			
-			mean = (score/(double)strlen(buff));
+			mean = (score/(double)bufflen);
 		}
+		
+		// Check conditions and print if satisfied
 		if(mean<=P && strlen(fastqline[2])>=lowerlength) {
 			printf("%s%s+\n%s", fastqline[1], fastqline[2], fastqline[4]);
 		}
 	}
+
+	// Close the FASTQ file
 	fclose(fastqfile);
+
 	return 0;
 }
