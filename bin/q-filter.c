50a4e5b2e7e88962dd15e8ac36860bb42f1f3af6
marcus-hoy-hansen <124658804+marcus-hoy-hansen@users.noreply.github.com>
2023-08-31 12:54:08 +0200

    Update q-filter.c

--- bin/q-filter.c
+++ bin/q-filter.c
@@ -5,9 +5,9 @@
 
 int main(int argc,char* argv[]) {
 
-        int lowerlength = 200;
-        double lowerqual = 20.0;
-        int runmode=1;
+	int lowerlength = 200;
+	double lowerqual = 20.0;
+	int runmode=1;
 
 	// Parse command-line arguments
 	if (argc > 2) lowerqual = atof(argv[2]);
@@ -21,20 +21,16 @@ int main(int argc,char* argv[]) {
 	double mean;
 	char buff[len], bufflen, currentchar,fastqline[4][len];
 
-	// Open the FASTQ file
+	// 1. Open the FASTQ file and count lines
 	FILE *fastqfile;
 	fastqfile = fopen(argv[1], "r");
 	while(!feof(fastqfile)) {
 		currentchar = fgetc(fastqfile);
-		
 		if(currentchar == '\n') lines++;
-	
-	}	
-
-	// Rewind the file to the beginning
+	}
 	rewind(fastqfile);
 	
-	// Process the FASTQ entries
+	// 2. Process the FASTQ entries
 	for (int i = 0; i < lines/4; i++) {
 		for (int j=1; j <= 4; ++j) {
 			fgets(buff, len, fastqfile);
@@ -44,7 +40,7 @@ int main(int argc,char* argv[]) {
 		score = 0;
 		bufflen = strlen(buff)-1;
 
-		// Calculate score and mean based on runmode
+		// 3. Calculate score and mean based on runmode
 		if(runmode==1) {
 			for (int u = 0; u < bufflen; u++) score += (buff[u]-33);
 			mean = -1*(score/bufflen)/10;
@@ -58,7 +54,7 @@ int main(int argc,char* argv[]) {
 			mean = (score/(double)bufflen);
 		}
 		
-		// Check conditions and print if satisfied
+		// 4. Check conditions and print if satisfied
 		if(mean<=P && strlen(fastqline[2])>=lowerlength) {
 			printf("%s%s+\n%s", fastqline[1], fastqline[2], fastqline[4]);
 		}
